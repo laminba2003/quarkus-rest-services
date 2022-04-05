c4d4877c21731f295c3f098f1b2847f032c8967c
mamadou lamine ba <laminba2003@gmail.com>
2022-04-05 22:38:46 +0000

    improvements

--- src/main/java/com/quarkus/training/controller/PersonController.java
+++ src/main/java/com/quarkus/training/controller/PersonController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.validation.Valid;
